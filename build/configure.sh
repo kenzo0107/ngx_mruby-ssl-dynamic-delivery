6f31c2c75d67301ba6993a9639a04af2b5a8b8e9
kenzo0107 <kenzo.tanaka0107@gmail.com>
2021-07-23 23:39:41 +0900

    nginx-build で利用するビルドオプションの見直し
    nginx-build で利用するビルドオプションを見直します。
    - 実際に利用していないオプションを削除
    - prefix を一般の nginx と同様 /etc/nginx にする
      - 調査時に分かりにくくならない様に一般的な設定に合わせる。
    
    ## nginx-build でのビルド方法
    
    ビルド時に以下の様にオプション指定しビルド設定を読み込む。
    ```
    nginx-build  -c configure.sh
    ```
    
    nginx-build ではビルドの設定がスクリプトで分けられる為、分離しています。
    Dockerfile に直接書くよりも分離することで可視性を高めます。
    https://github.com/cubicdaiya/nginx-build#custom-configuration
    
    ## nginx-build で nginx をビルドする理由
    
    nginx-build によって PCRE や ZLIB の設定が簡単にできる為、採用しました。
    参考: https://qiita.com/cubicdaiya/items/a0411c190f043a85ad2f
    
    ## 各 Nginx ビルドオプションの設定について
    
    * --with-pcre-jit : JITコンパイルサポートでPCREをビルドします。
      - PCRE は nginx の rewrite で利用する為、導入しておきます。現状利用予定はないですが、使用する可能性が高い為です。
      - ちなみに、 PCRE を外す場合は --without-​http_rewrite_module の設定が必要
    * --with-http_v2_module : HTTP/2 サポートする設定で HTTP/2 通信する際に必須です。
    * --with-http_ssl_module : SSL サポートする設定で必須です。
    * --with-http_gunzip_module : gzip"符号化メソッドをサポートしないクライアントのために"Content-Encoding: *zip"の応答を解凍するフィルターとして機能します。
    * --with-http_gzip_static_module : ".gz"ファイル拡張子のあらかじめ圧縮されたファイル送信時に利用します。
    * --with-http_secure_link_module : リクエストされたリンクの真偽をチェックし、許可されていないアクセスからリソースを保護し、リンクの生存期間を制限します。
    * --with-stream : TCP プロキシ処理で必要です。現状の仕様上、S3 へのプロキシ設定は https のみできる様に S3 Bucket Policy で制御していますが、他のプロキシもあり得るので設定しておきます。
    * --with-stream_ssl_module : TLS プロキシ処理で必要です。 S3 へのプロキシする際に https のみリクエストを処理する様にしている為、必須です。
    * --without-stream_limit_conn_module : 1つのIPアドレスからの接続数の制限を無効化します。
      - 複数タスクで構成される関係上、意図せず、 VPC 内のリクエストをブロックしてしまわない様に設定を外しておきます。
    * --add-module=../ngx_mruby/dependence/ngx_devel_kit : nginx_mruby 組み込み

--- build/configure.sh
+++ build/configure.sh
@@ -1,17 +1,13 @@
 #!/bin/sh
 
 ./configure --with-debug \
- --prefix=/usr/local/nginx \
+ --prefix=/etc/nginx \
  --with-pcre-jit \
- --with-http_stub_status_module \
  --with-http_v2_module \
  --with-http_ssl_module \
  --with-http_realip_module \
- --with-http_addition_module \
- --with-http_sub_module \
  --with-http_gunzip_module \
  --with-http_gzip_static_module \
- --with-http_random_index_module \
  --with-http_secure_link_module \
  --with-stream \
  --with-stream_ssl_module \
